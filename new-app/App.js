bf913f339ba687fa1b75abde4627c154b8efc151
imamfahr <imamfahr@gmail.com>
2020-10-01 10:56:00 +0700

    add new-app

---
+++ new-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Werld</Text>
+      <Text>hey baby, I'm trying to create a change</Text>
+      <Button title='press dis!'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
